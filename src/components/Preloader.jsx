58d34889801f1b725618fe5a128acedd89d8fd20
Katelynn Anderson <kma0284@gmail.com>
2026-08-23 11:14:56 -0400

    preloader, fonts move to vendor

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,11 +1,11 @@
-import "../blocks/Preloader.css";
+import "./Preloader.css";
 
 function Preloader() {
   return (
-    <section className="preloader">
-      <div className="preloader__spinner" />
-      <p className="preloader__text">Searching for news...</p>
-    </section>
+    <div className="preloader">
+      <div className="circle-preloader"></div>
+      <p>Searching for news...</p>
+    </div>
   );
 }
 
