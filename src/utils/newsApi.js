c4b804c4f39e6fdb1a9f5f389739ad141365e4fc
Katelynn Anderson <kma0284@gmail.com>
2026-08-30 08:48:15 -0400

    Use NewsAPI proxy for production

--- src/utils/newsApi.js
+++ src/utils/newsApi.js
@@ -1,10 +1,26 @@
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
-const BASE_URL = "https://newsapi.org/v2/everything";
+
+const BASE_URL = import.meta.env.PROD
+  ? "https://nomoreparties.co/news/v2/everything"
+  : "https://newsapi.org/v2/everything";
 
 export const getNews = (query) => {
-  return fetch(
-    `${BASE_URL}?q=${encodeURIComponent(query)}&apiKey=${API_KEY}`,
-  ).then((res) => {
+  const today = new Date();
+  const sevenDaysAgo = new Date();
+
+  sevenDaysAgo.setDate(today.getDate() - 7);
+
+  const formatDate = (date) => date.toISOString().split("T")[0];
+
+  const params = new URLSearchParams({
+    q: query,
+    apiKey: API_KEY,
+    from: formatDate(sevenDaysAgo),
+    to: formatDate(today),
+    pageSize: "100",
+  });
+
+  return fetch(`${BASE_URL}?${params.toString()}`).then((res) => {
     if (!res.ok) {
       throw new Error(`Request failed: ${res.status}`);
     }
